2de230fd46e37253d9da2a4b2364d2899ce932d5
Johan Kardell <johan.kardell@ericsson.com>
2014-06-13 13:20:13 +0200

    Added Code metrics dummy files

--- app/shell.js
+++ app/shell.js
@@ -6,7 +6,8 @@ define(function (require) {
      activate: function () {
        router.map([
          { route: '', title:'Home', moduleId: 'home', nav: true },
-         { route:'rainier', title:'Mount Rainier', moduleId:'rainier', nav:true }
+         { route:'rainier', title:'Mount Rainier', moduleId:'rainier', nav:true },
+         { route:'metrics', title:'Code metrics', moduleId:'metrics', nav:true }
        ]).buildNavigationModel();
  
        return router.activate();
